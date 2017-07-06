2cc926425d2d07730eb1f127c2f7ad6f554eafe4
Caleb Sharp <epicsharp11@gmail.com>
2017-07-06 09:10:22 -0700

    Look nice pls

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Grid, Row } from 'react-bootstrap';
 import logo from './logo.svg';
 import './index.css';
 
 import Uploader from '../Uploader';
+import CourseTypeBox from '../CourseTypeBox';
+import CourseAuthorBox from '../CourseAuthorBox';
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
-        <Button bsStyle="primary">Upload</Button>
+        <h1>LMS Inspector</h1>
+        <Row>
+          <Col xs={2}>
+            <CourseTypeBox />
+          </Col>
+          <Col xs={2}>
+            <CourseAuthorBox />
+          </Col>
+        </Row>
         <Uploader />
+        <Button bsStyle="primary">Upload</Button>
       </div>
     );
   }
