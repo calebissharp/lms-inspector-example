c1a046e99486591959fca1e2dc80a541fe0b554c
Caleb Sharp <epicsharp11@gmail.com>
2017-07-06 18:08:04 -0700

    Refactor FileInspector to be more readable and to have a common api for zips+gzips

--- src/util/fileInspector.js
+++ src/util/fileInspector.js
@@ -1,73 +1,65 @@
 import JSZip from 'jszip';
 import pako from 'pako';
-
-const fileTypes = {
-  'zip': [0x50, 0x4b, 0x03, 0x04],
-  'gzip': [0x1f, 0x8b, 0x08],
-}
+import { TextDecoder, TextEncoder } from 'text-encoding';
 
 export const FileInspector = {
-  inspect(file) { 
-    return FileInspector.getFileBytes(file)
-      .then(FileInspector.uncompress);
-  },
-  determineCompression(arrayBuffer) {
+  inspect: file => new Promise((resolve, reject) => {
+    FileInspector.convertFileToArrayBuffer(file)
+      .then(buffer => {
+        const compression = FileInspector.determineCompression(buffer);
+
+        if (compression === 'zip') {
+          return FileInspector.uncompressZip(buffer)
+        } else if (compression === 'gzip') {
+          return FileInspector.uncompressGzip(buffer)
+        }
+      })
+      .then(FileInspector.checkForLMS)
+      .then(resolve);
+  }), 
+  convertFileToArrayBuffer: file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = () => reject('Unable to read file');
+    reader.readAsArrayBuffer(file);
+  }),
+  determineCompression: arrayBuffer => {
     const bytes = new Uint8Array(arrayBuffer);
 
-    for (const type in fileTypes) {
-      if (!fileTypes.hasOwnProperty(type)) continue;
-
-      const firstByte = fileTypes[type][0];
-      if(firstByte === bytes[0]) { return type }
-    }
+    switch(bytes[0]) {
+      case 0x50: return 'zip';
+      case 0x1f: return 'gzip';
+    };
   },
-  getFileBytes(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-
-      reader.onloadend = evt => {
-        if (evt.target.readyState === FileReader.DONE) {
-          resolve(reader.result);
-        } else {
-          reject('could not get bytes');
-        }
-      };
-
-      reader.onerror = reject;
-
-      reader.readAsArrayBuffer(file);
+  uncompressZip: arrayBuffer => new Promise((resolve, reject) => {
+    const zip = new JSZip();
+    zip.loadAsync(arrayBuffer).then(() => {
+      const files = [];
+      zip.forEach((_, f) => files.push(f.name));
+      resolve(files);
     });
-  },
-  uncompress(arrayBuffer, compressionType) {
-    return new Promise((resolve, reject) => {
-      const bytes = new Uint8Array(arrayBuffer);
+  }),
+  uncompressGzip: arrayBuffer => new Promise((resolve, reject) => {
+    const uncompressed = pako.inflate(arrayBuffer);
+    const string = new TextDecoder('utf-8').decode(uncompressed)
+      .substring(0, 1300) // take only the first part of the file that contains filenames
+      .replace(/\t|\d+|(f|c|d)\t|\?/g, '') // magical regexp to clean up messy uncompressed string
+      .split(/\n/g) // split the string into an array of filenames
+      .slice(1); // remove big empty first entry
+    resolve(string);
+  }),
+  checkForLMS: filenames => {
+    let result = '';
 
-      // if no compression type is given, determine compression type
-      if (!compressionType) {
-        compressionType = FileInspector.determineCompression(arrayBuffer);
-      }
-
-      if (compressionType === 'gzip') {
-        const result = pako.inflate(bytes);
-        resolve(result);
-      } else if (compressionType === 'zip') {
-        const zip = new JSZip();
-
-        zip.loadAsync(arrayBuffer)
-          .then(() => {
-            zip.forEach((relativePath, zipEntry) => {
-              const lms = FileInspector.checkForLMS(zipEntry);
-              if(lms) resolve(lms);
-            });
-          });
-      }
+    filenames.forEach(name => {
+      if(name.includes('course_settings')) result = 'canvas';
+      if(name.includes('moodle') || name.includes('completion.xml')) result = 'moodle';
+      if(name.includes('bb-')) result = 'blackboard';
+      if(name.includes('brainhoneymanifest')) result = 'buzz';
+      if(name.includes('d2l')) result = 'd2l';
     });
-  },
-  checkForLMS(file) {
-    const name = file.name;
-    if(name.includes('moodle')) return 'moodle';
-    if(name.includes('bb-')) return 'blackboard';
-    if(name.includes('brainhoneymanifest')) return 'buzz';
-    if(name.includes('d2l')) return 'd2l';
-  },
+
+    return result;
+  }
 };
